5769bb53c2be6702e88f4a00a804f99a2c4b78ce
Your Namea <221810140@stis.ac.id>
2022-06-20 20:53:55 +0700

    Added dataset

---
+++ R/simulatedataIPM.R
@@ -0,0 +1,16 @@
+#' @title  Sample Data for simulate analysis data (Using IPM 2019)
+#'
+#' @description A real data of Human Development Index (Indeks Pembangunan Manusia) of West Java, Indonesia 2019
+#'
+#' @format A \code{tibble} with 27 observation as region and 4 column as variables, which are:
+#' \describe{
+#' \item{"AHH"}{a value of Life expectancy (Angka Harapan Hidup)}
+#' \item{"HLS"}{a value of Expected Years of Schooling (Harapan Lama Sekolah)}
+#' \item{"RLS"}{a value of Mean Years of Schooling (Rata-rata Lama Sekolah)}
+#' \item{"Pengeluaran"}{a value of Expenditure (Pengeluaran)}
+#' }
+#'
+#' @source \url{https://www.bps.go.id/}
+#'
+#'
+"simulatedataIPM"
